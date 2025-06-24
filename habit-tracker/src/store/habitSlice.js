67a717e0c9fc05d189d535f78fe80fcd05336c99
surya pratap singh <surya2pratap0singh04@gmail.com>
2025-06-25 00:10:33 +0530

    initialization

---
+++ habit-tracker/src/store/habitSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+
+}
+
+export const habitSlice = createSlice({
+    name: 
+    initialState,
+    reducers: {
+        
+    }
+})
