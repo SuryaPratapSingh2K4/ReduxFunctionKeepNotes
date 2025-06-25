c3920ece57cd116210c863ae27a3de1cd995f3a9
surya pratap singh <surya2pratap0singh04@gmail.com>
2025-06-25 08:17:11 +0530

    redux setup

--- habit-tracker/src/store/habitSlice.js
+++ habit-tracker/src/store/habitSlice.js
@@ -1,13 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit"
 
-const initialState = {
+const LoadDataFromLocalStorage = JSON.parse(localStorage.getItem('habits')) || [];
 
+const SaveDataToLocalStorage = (state) => {
+    localStorage.setItem('habits',JSON.stringify(state));
+}
+
+const initialState = {
+    habits: LoadDataFromLocalStorage
 }
 
 export const habitSlice = createSlice({
-    name: 
+    name: "habit",
     initialState,
     reducers: {
-        
+        addHabit: (state,action) => {
+            state.push(
+                {
+                    id: Date.now(),
+                    title: action.payload,
+                    completedays: []
+                }
+            )
+            SaveDataToLocalStorage(state)
+        },
+
+        toggleHabits : (state,action) => {
+            const {id,date} = action.payload
+            const habit = state.habits((h) => h.id === id);
+            if(habit){
+                if(habit.completedays.includes(date)){
+                    habit.completedays = habit.completedays.filter(d => d !== date)
+                }else{
+                    habit.completedays.push(date)
+                }
+            }
+            SaveDataToLocalStorage(state)
+        },
+
+        deleteHbaits: (state,action) => {
+            const filtered =  state.filter((h) => h.id !== action.payload)
+            SaveDataToLocalStorage(filtered)
+        }
     }
 })
