43d907239f3326a0b4a1d77f5905e88454007c3b
surya pratap singh <surya2pratap0singh04@gmail.com>
2025-07-18 21:57:51 +0530

    members

--- expense-tracker/src/pages/GroupDetails.jsx
+++ expense-tracker/src/pages/GroupDetails.jsx
@@ -1,22 +1,33 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom'
+import { addMember, deleteGroup, deleteMember, editMemberName } from '../store/groupSlice';
 
 function GroupDetails() {
     const { groupId } = useParams();
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    const group = useSelector(state.group.groups.find((g) => g.id === groupId))
+    const group = useSelector((state) => state.group.groups.find((g) => g.id === groupId))
     const [newMember, setNewMember] = useState("");
     const [newExpense, setNewExpense] = useState("");
 
     const [editingGroup, setEditingGroup] = useState(false);
     const [editGroupName, setEditGroupName] = useState("")
 
+    const [editingName, setEditingName] = useState(false);
+    const [editName, setEditName] = useState("");
+
     const handleEditGroup = () => {
-        dispatch(editGroupName({groupId, newName: editGroupName}))
+        dispatch(editGroupName({ groupId, newName: editGroupName }))
         setEditingGroup(false);
         setEditGroupName("");
+    }
+
+    const handleNewMember = () => {
+        if (newMember.trim()) {
+            dispatch(addMember({ groupId, memberName: newMember }))
+            setNewMember("");
+        }
     }
 
     return (
@@ -52,10 +63,89 @@ function GroupDetails() {
                             <h2 className='text-2xl font-semibold'>{group.name}</h2>
                         )
                 }
+                <div>
+                    <button
+                        onClick={() => setEditingGroup(true)}
+                        className='text-sm bg-yellow-600 hover:bg-yellow-500 text-black px-3 py-2 rounded-lg'
+                    >
+                        Edit Name
+                    </button>
+                    <button
+                        onClick={() => {
+                            dispatch(deleteGroup(groupId));
+                            navigate('/')
+                        }}
+                        className='text-sm bg-red-600 hover:bg-red-500 text-white px-3 py-2 rounded-lg'
+                    >
+                        Delete Group
+                    </button>
+                </div>
             </div>
+
 
             <div className='bg-gray-800 p-4 rounded-lg'>
                 <h3 className='text-xl font-semibold'>Members of {group.name}</h3>
+                <div className='flex gap-2 mb-3'>
+                    <input
+                        type="text"
+                        placeholder='Add a new member'
+                        value={newMember}
+                        onChange={(e) => setNewMember(e.target.value)}
+                        className='px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-lg w-full'
+                    />
+                    <button
+                        onClick={handleNewMember}
+                        className='bg-green-600 hover:bg-green-500 text-white px-3 py-2 rounded-lg'
+                    >
+                        +
+                    </button>
+                    <div>
+                        <ul>
+                            {
+                                group.members.map((member) => (
+                                    <li key={member.id} className='flex justify-center mb-1'>
+                                        {
+                                            editingName ? (
+                                                <div className='flex items-center'>
+                                                    <input
+                                                    type="text"
+                                                    value={editName}
+                                                    onChange={(e) => setEditName(e.target.value)}
+                                                    className='px-2 py-1 bg-gray-700 text-white border border-gray-600 rounded-lg ml-2'
+                                                />
+                                                <button
+                                                className='bg-blue-600 px-3 py-2 rounded-lg'
+                                                onClick={() => {
+                                                    dispatch(editMemberName({ groupId, memberId: member.id, newName: editName }));
+                                                    setEditingName(false);
+                                                    setEditName("");
+                                                }}
+                                                >
+                                                    Save
+                                                </button>
+                                                </div>
+                                            ) : (
+                                                <span>{member.name}</span>
+                                            )
+                                        }
+                                        <button
+                                        className='text-sm bg-yellow-600 hover:bg-yellow-500 text-black px-3 py-2 rounded-lg'
+                                        onClick={() => setEditingName(true)}
+                                        >
+                                            Edit
+                                        </button>
+                                        <button
+                                        className='text-red-600 hover:text-red-800 text-xs'
+                                        onClick={() => {dispatch(deleteMember({groupId,memberId: member.id}))}}
+                                        >
+                                            Remove
+                                        </button>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </div>
             </div>
 
         </div>
